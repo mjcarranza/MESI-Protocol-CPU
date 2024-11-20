704020d2830442383de5d80af7d3419069a11a47
AlpizarGregory <gregory.ac@estudiantec.cr>
2024-11-19 22:50:13 -0600

    Added Server connection

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,15 @@
 package src;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
@@ -20,33 +24,203 @@ public class Controller implements Initializable {
     private TableView<Statistic> statsTable;
     @FXML
     private GridPane root;
-    @FXML
-    private Line dataBus;
 
     private Server server;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<Statistic> statsData = FXCollections.observableArrayList(
-                new Statistic("Cache Misses"),
-                new Statistic("Cache Hits"),
-                new Statistic("Read Requests"),
-                new Statistic("Write Requests")
+                new Statistic("Cache Misses", 0),
+                new Statistic("Cache Hits", 0),
+                new Statistic("Read Requests", 0),
+                new Statistic("Write Requests", 0)
         );
 
         this.statsTable.setItems(statsData);
 
-        System.out.println(5);
-
-        this.dataBus.setStroke(Color.RED);
-
-        /*try {
-            server = new Server(new ServerSocket(8888));
+        try {
+            this.server = new Server(new ServerSocket(8888));
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Error creating server");
+            System.out.println("Error creating server.");
         }
 
-        server.receiveMessage();*/
+
+        this.server.receiveMessage(this.root);
+    }
+
+    public static void handleMessage(String message, GridPane root) {
+        if (message != null && !message.isEmpty()) {
+            String firstChar = message.substring(0, 1);
+
+            switch (firstChar) {
+                case "C":
+                    if (message.substring(1, 2).equals("a")){
+                        handleStats(message, root);
+                    } else {
+                        handleCaches(message, root);
+                    }
+                    break;
+                case "M":
+                    handleMainMemory(message, root);
+                    break;
+                case "B":
+                    handleBuses(message, root);
+                    break;
+                default:
+                    handleStats(message, root);
+            }
+        }
+    }
+
+    public static void handleCaches(String message, GridPane root) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                HBox cpuRow = (HBox) root.getChildren().get(5);
+
+                String secondChar = message.substring(1, 2);
+                Integer cacheNumber = Integer.valueOf(secondChar);
+                VBox cpuAndCacheBox = (VBox) cpuRow.getChildren().get(cacheNumber - 1);
+                VBox cacheDataBox = (VBox) cpuAndCacheBox.getChildren().get(0);
+                Label newLabel = null;
+                Boolean isInCache = false;
+
+                Integer index = 6;
+                String state = message.substring(4, 5);
+                String address = "";
+                String nextChar;
+                while (true) {
+                    nextChar = message.substring(index, index+1);
+                    if (!nextChar.equals("|")) {
+                        address = address + nextChar;
+                    } else {
+                        index++;
+                        break;
+                    }
+
+                    index++;
+                }
+
+                String data = "";
+                while (index < message.length()) {
+                    nextChar = message.substring(index, index+1);
+                    data = data + nextChar;
+                    index++;
+                }
+
+                String labelString = "State: " + state + " | Addr: " + address + " | Data: " + data;
+
+                for (int i = 0; i < 2; i++) {
+                    Label currentLabel = (Label) cacheDataBox.getChildren().get(i);
+                    if (currentLabel.getText().contains(address)) {
+                        newLabel = currentLabel;
+                        isInCache = true;
+                        break;
+                    }
+                }
+
+                if (isInCache) {
+                    newLabel.setText(labelString);
+                } else {
+                    Label label0 = (Label) cacheDataBox.getChildren().get(0);
+                    Label label1 = (Label) cacheDataBox.getChildren().get(1);
+
+                    label1.setText(label0.getText());
+                    label0.setText(labelString);
+                }
+
+            }
+        });
+    }
+
+    public static void handleMainMemory(String message, GridPane root) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                HBox centeredMemoryBox = (HBox) root.getChildren().get(1);
+                VBox memoryDataBox = (VBox) centeredMemoryBox.getChildren().getFirst();
+                Integer index = 0;
+                String address = message.substring(3, 5);
+                String data = message.substring(6);
+                Label currentLabel = null;
+
+                for (int i = 0; i < 4; i++) {
+                    currentLabel = (Label) memoryDataBox.getChildren().get(i);
+                    if (currentLabel.getText().contains(address)) {
+                        break;
+                    }
+                }
+
+                String newLabel = "Addr: " + address + ",   Data: " + data;
+
+                if (currentLabel != null) {
+                    currentLabel.setText(newLabel);
+                }
+            }
+        });
+    }
+
+    public static void handleBuses(String message, GridPane root) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                VBox busBox = (VBox) root.getChildren().get(3);
+
+                String busType = message.substring(1);
+                Integer busIndex = 0;
+                Color busColor = Color.BLACK;
+
+                switch (busType) {
+                    case "A":
+                        busIndex = 3;
+                        busColor = Color.GREEN;
+                        break;
+                    case "D":
+                        busIndex = 1;
+                        busColor = Color.RED;
+                        break;
+                    case "S":
+                        busIndex = 5;
+                        busColor = Color.BLUEVIOLET;
+                        break;
+                }
+
+                Line busLine = (Line) busBox.getChildren().get(busIndex);
+                busLine.setStroke(busColor);
+            }
+        });
+    }
+
+    public static void handleStats(String message, GridPane root) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                String stat = message.substring(10);
+                Integer rowIndex = 0;
+
+                switch (stat){
+                    case "Cache miss!":
+                        rowIndex = 0;
+                        break;
+                    case "Cache hit!":
+                        rowIndex = 1;
+                        break;
+                    case "Read request":
+                        rowIndex = 2;
+                        break;
+                    case "Write request":
+                        rowIndex = 3;
+                        break;
+                }
+
+                VBox statsBox = (VBox) root.getChildren().get(7);
+                TableView<Statistic> statsTable = (TableView<Statistic>) statsBox.getChildren().get(1);
+                Statistic statistic = (Statistic) statsTable.getItems().get(rowIndex);
+                Integer oldValue = statistic.getValue();
+                statistic.setValue(oldValue + 1);
+                statsTable.refresh();
+            }
+        });
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,59 @@
 package src;
 
+import javafx.scene.layout.GridPane;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+
+    public Server(ServerSocket serverSocket) {
+        try {
+            this.serverSocket = serverSocket;
+            this.socket = serverSocket.accept();
+            System.out.println("Connected");
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("Error creating server.");
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveMessage(GridPane root) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (socket.isConnected()) {
+                    try {
+                        String message = bufferedReader.readLine();
+                        Controller.handleMessage(message, root);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Error receiving message from backend");
+                        closeEverything(socket, bufferedReader);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
